94b83eaa4d3d219ae60d2aa608fc9ee9a3c62b24
rocketmike12 <mg.korop@gmail.com>
2025-10-05 19:07:25 +0300

    nvim hint symbol changed

--- nvim/.config/nvim/lua/config/diagnostics.lua
+++ nvim/.config/nvim/lua/config/diagnostics.lua
@@ -25,7 +25,7 @@ vim.api.nvim_create_autocmd("CursorHold", {
 vim.fn.sign_define("DiagnosticSignError",  {text = "", texthl = "DiagnosticError"})
 vim.fn.sign_define("DiagnosticSignWarn",   {text = "", texthl = "DiagnosticWarn"})
 vim.fn.sign_define("DiagnosticSignInfo",   {text = "", texthl = "DiagnosticInfo"})
-vim.fn.sign_define("DiagnosticSignHint",   {text = "", texthl = "DiagnosticHint"})
+vim.fn.sign_define("DiagnosticSignHint",   {text = "H", texthl = "DiagnosticHint"})
 
 vim.cmd [[
 	hi DiagnosticError guifg=#ff4d6d guibg=NONE
