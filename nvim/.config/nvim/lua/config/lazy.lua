e99e8192092270baa234ac3adadcb7dfdff3f2af
rocketmike12 <mg.korop@gmail.com>
2025-11-26 19:48:06 +0200

    nvim plugin updates
     - removed firenvim
     - added nvim-surround

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -204,7 +204,18 @@ require("lazy").setup({
 
 	-- { 'edluffy/hologram.nvim', opts={} },
 
-	{ 'glacambre/firenvim', build = ":call firenvim#install(0)" },
+	-- { 'glacambre/firenvim', build = ":call firenvim#install(0)" },
+
+	{
+		"kylechui/nvim-surround",
+		-- version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
 
 	-- THEMES
 	{ 'Mofiqul/dracula.nvim', opts = {} },
