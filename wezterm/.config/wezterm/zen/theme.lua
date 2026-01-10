1a1dec922074d4c1f3ddf93c1691b1da5ef40b0a
rocketmike12 <mg.korop@gmail.com>
2026-01-10 18:27:18 +0200

    wezterm theming transferred

---
+++ wezterm/.config/wezterm/zen/theme.lua
@@ -0,0 +1,36 @@
+local theme = {}
+
+theme.colors = {
+	background = "#000000",
+	foreground = "#ffffff",
+
+	cursor_bg = "#ffffff",
+	cursor_fg = "#000000",
+	cursor_border = "#ffffff",
+
+	split = "#ffffff",
+
+	ansi = {
+		"#000000",
+		"#b9466e",
+		"#9f6081",
+		"#AD8B85",
+		"#7a609f",
+		"#84609f",
+		"#84609f",
+		"#ddddee"
+	},
+
+	brights = {
+		"#1f1626",
+		"#c0597d",
+		"#a9708d",
+		"#C2A8A3",
+		"#8770a9",
+		"#9070a9",
+		"#9070a9",
+		"#eeeeff"
+	}
+}
+
+return theme
