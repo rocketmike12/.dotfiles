695a612647380786f81faf2e892bdc4eae8c682a
rocketmike12 <mg.korop@gmail.com>
2026-01-11 14:17:09 +0200

    improved logging

--- rm12-hypr/.config/rm12-hypr/theme.sh
+++ rm12-hypr/.config/rm12-hypr/theme.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 selection=""
 
 for f in $(ls "$HOME/.config/rm12-hypr/themes/"); do
@@ -14,7 +12,7 @@ THEME=$(echo -e $selection | rofi -dmenu -p "Theme:")
 
 [ ! -d "$HOME/.config/rm12-hypr/themes/$1" ] && exit 1
 
-echo "$THEME exists"
+echo "theme: $THEME"
 
 echo $THEME > ~/.config/rm12-hypr/theme
 
@@ -22,7 +20,7 @@ BASE="$HOME/.config/rm12-hypr/themes/$THEME"
 WALLPAPER="$HOME/.config/rm12-hypr/wallpapers/$THEME/wallpaper"
 
 if [[ -f "$HOME/.config/hypr/rm12-hypr.lock" && -d "$BASE/hypr" ]]; then
-	echo "hypr"
+	echo "setting hypr"
 
 	ln -sf "$BASE/hypr/look_and_feel.conf" "$HOME/.config/hypr/look_and_feel.conf"
 	ln -sf "$WALLPAPER" "$HOME/.config/rm12-hypr/wallpaper"
@@ -32,7 +30,7 @@ if [[ -f "$HOME/.config/hypr/rm12-hypr.lock" && -d "$BASE/hypr" ]]; then
 fi
 
 if [[ -f "$HOME/.config/waybar/rm12-hypr.lock" && -d "$BASE/waybar" ]]; then
-	echo "waybar"
+	echo "setting waybar"
 
 	ln -sf "$BASE/waybar/config.jsonc" "$HOME/.config/waybar/config.jsonc"
 	ln -sf "$BASE/waybar/style.css" "$HOME/.config/waybar/style.css"
@@ -42,13 +40,13 @@ if [[ -f "$HOME/.config/waybar/rm12-hypr.lock" && -d "$BASE/waybar" ]]; then
 fi
 
 if [[ -f "$HOME/.config/rofi/rm12-hypr.lock" && -d "$BASE/rofi" ]]; then
-	echo "rofi"
+	echo "setting rofi"
 
 	ln -sf "$BASE/rofi/config.rasi" "$HOME/.config/rofi/config.rasi"
 fi
 
 if [[ -f "$HOME/.config/alacritty/rm12-hypr.lock" && -d "$BASE/alacritty" ]]; then
-	echo "alacritty"
+	echo "setting alacritty"
 
 	ln -sf "$BASE/alacritty/theme.toml" "$HOME/.config/alacritty/theme.toml"
 
@@ -56,16 +54,15 @@ if [[ -f "$HOME/.config/alacritty/rm12-hypr.lock" && -d "$BASE/alacritty" ]]; th
 fi
 
 if [[ -f "$HOME/.config/kitty/rm12-hypr.lock" && -d "$BASE/kitty" ]]; then
-	echo "kitty"
+	echo "setting kitty"
 
 	ln -sf "$BASE/kitty/theme.conf" "$HOME/.config/kitty/theme.conf"
 
 	killall -q -SIGUSR1 kitty
 fi
 
-
 if [[ -f "$HOME/.config/wezterm/rm12-hypr.lock" && -d "$BASE/wezterm" ]]; then
-	echo "wezterm"
+	echo "setting wezterm"
 
 	ln -sf "$BASE/wezterm/theme.lua" "$HOME/.config/wezterm/theme.lua"
 
@@ -73,7 +70,7 @@ if [[ -f "$HOME/.config/wezterm/rm12-hypr.lock" && -d "$BASE/wezterm" ]]; then
 fi
 
 if [[ -f "$HOME/.mozilla/firefox/mike/chrome/rm12-hypr.lock" && -d "$BASE/firefox" ]]; then
-	echo "firefox"
+	echo "setting firefox"
 
 	ln -sf "$BASE/firefox/theme.css" "$HOME/.mozilla/firefox/mike/chrome/theme.css"
 fi
