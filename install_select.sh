ea654ffa5c580e86b8f44ec68acaca7ab6a225a2
rocketmike12 <mg.korop@gmail.com>
2026-01-18 10:39:01 +0200

    select installer wip

--- install_select.sh
+++ install_select.sh
@@ -7,48 +7,50 @@ modules=("scripts" "wallpapers" "bash" "zsh" "alacritty" "kitty" "wezterm" "nvim
 profiles=("hyprland (hypr + waybar + rofi + scripts)" "nvim (nvim + tmux + alacritty)" "all" "DONE")
 
 PS3="select installation category: "
-select opt1 in "${categories[@]}"; do
-	case "$opt1" in
-		modules)
-			PS3="select module to install: "
-			select opt2 in "${modules[@]}"; do
-				case "$opt2" in 
-					DONE)
-						break
-						;;
-					*)
-						echo "$opt2"
-						;;
-				esac
-			done
+selected=$(
+	select opt1 in "${categories[@]}"; do
+		case "$opt1" in
+			modules)
+				PS3="select module to install: "
+				select opt2 in "${modules[@]}"; do
+					case "$opt2" in 
+						DONE)
+							break
+							;;
+						*)
+							echo "$opt2"
+							;;
+					esac
+				done
 
-			break
-			;;
+				break
+				;;
 
-		profiles)
-			PS3="select profile to install: "
-			select opt2 in "${profiles[@]}"; do
-				case "$REPLY" in 
-					1)
-						echo "hypr waybar rofi scripts"
-						;;
-					2)
-						echo "nvim tmux alacritty"
-						;;
-					3)
-						echo "scripts wallpapers bash zsh alacritty kitty wezterm nvim hypr waybar rofi firefox"
-						;;
-					4)
-						break
-						;;
-				esac
-			done
+			profiles)
+				PS3="select profile to install: "
+				select opt2 in "${profiles[@]}"; do
+					case "$REPLY" in 
+						1)
+							echo "hypr waybar rofi scripts"
+							;;
+						2)
+							echo "nvim tmux alacritty"
+							;;
+						3)
+							echo "scripts wallpapers bash zsh alacritty kitty wezterm nvim hypr waybar rofi firefox"
+							;;
+						4)
+							break
+							;;
+					esac
+				done
 
-			break
-			;;
+				break
+				;;
 
-		DONE)
-			break
-			;;
-	esac
-done
+			DONE)
+				break
+				;;
+		esac
+	done
+)
