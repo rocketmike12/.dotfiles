d61b8c4653e74c5ea4671fdb20e8802707a52647
rocketmike12 <mg.korop@gmail.com>
2026-01-19 19:25:46 +0200

    changed split keybiinds; changed resize size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -118,22 +118,22 @@ config.keys = {
 	{
 		key = 'h',
 		mods = 'CTRL|SHIFT',
-		action = wezterm.action.AdjustPaneSize { 'Left', 2 }
+		action = wezterm.action.AdjustPaneSize { 'Left', 1 }
 	},
 	{
 		key = 'j',
 		mods = 'CTRL|SHIFT',
-		action = wezterm.action.AdjustPaneSize { 'Down', 2 }
+		action = wezterm.action.AdjustPaneSize { 'Down', 1 }
 	},
 	{
 		key = 'k',
 		mods = 'CTRL|SHIFT',
-		action = wezterm.action.AdjustPaneSize { 'Up', 2 }
+		action = wezterm.action.AdjustPaneSize { 'Up', 1 }
 	},
 	{
 		key = 'l',
 		mods = 'CTRL|SHIFT',
-		action = wezterm.action.AdjustPaneSize { 'Right', 2 }
+		action = wezterm.action.AdjustPaneSize { 'Right', 1 }
 	}
 }
 
